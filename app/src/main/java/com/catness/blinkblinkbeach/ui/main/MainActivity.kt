e41ed5ca2fba4e7ed96813ad3c77fbfd248346fd
CHOO XIANG LUN <xlchoo25@gmail.com>
2022-12-17 02:29:45 +0800

    Set up top app bar and home fragment - set up top app bar - set up home fragment - auto hide top app bar and bottom nav bar - change to use navigation component to control bottom nav - problem: awkward transition between fragment with the which needs auto hide top app bar / bottom nav bar

--- app/src/main/java/com/catness/blinkblinkbeach/ui/main/MainActivity.kt
+++ app/src/main/java/com/catness/blinkblinkbeach/ui/main/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.catness.blinkblinkbeach.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.catness.blinkblinkbeach.NavGraphDirections
 import com.catness.blinkblinkbeach.R
 import com.catness.blinkblinkbeach.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,6 +22,8 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().setKeepOnScreenCondition {
@@ -21,11 +31,25 @@ class MainActivity : AppCompatActivity() {
         }
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        appBarConfiguration = AppBarConfiguration(
+            // fragments placed inside this set will be consider as top-level destination
+            setOf(
+                R.id.homeFragment,
+                R.id.reportFragment,
+                R.id.notificationFragment,
+                R.id.profileFragment
+            )
+        )
+
+        binding.apply {
+            setContentView(root)
+            setSupportActionBar(topAppBar)
+        }
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         viewModel.isUserAuthenticated.observe(this) { isUserAuthenticated ->
             if (isUserAuthenticated == true) {
@@ -36,26 +60,39 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        //Bottom Nav Bar Navigation
-        binding.bottomNavigationMenu.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    navController.navigate(R.id.homeFragment)
-                    true
-                }
-                R.id.report -> {
-                    navController.navigate(R.id.reportFragment)
-                    true
-                }
-                R.id.notification -> {
-                    navController.navigate(R.id.notificationFragment)
-                    true
-                }
-                R.id.profile -> {
-                    navController.navigate(R.id.profileFragment)
-                    true
-                }
-                else -> false
+        // apply navigation on the action bar
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // set up top app bar visibility listener
+        viewModel.setVisibilityTopAppBar(navController, binding.topAppBar)
+
+        // set up bottom nav bar
+        NavigationUI.setupWithNavController(binding.bottomNavigationMenu, navController)
+
+        // set up bottom nav visibility listener
+        viewModel.setVisibilityBottomNavBar(navController, binding.bottomNavigationMenu)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // make the back button on action bar functional
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                val action = NavGraphDirections.actionGlobalSignInFragment()
+                viewModel.onLogOutButtonClick()
+                navController.navigate(action)
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
             }
         }
     }
